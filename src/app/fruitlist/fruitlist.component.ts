759f2835ca1d7d8ec6a8ed1ce45a3b288cbace90
MihaelaAghirculesei <aghirculesei@gmail.com>
2025-03-18 23:39:44 +0100

    Added headline and fixed line endings

--- src/app/fruitlist/fruitlist.component.ts
+++ src/app/fruitlist/fruitlist.component.ts
@@ -1,26 +1,23 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, } from '@angular/core';
+import { SinglefruitComponent } from './singlefruit/singlefruit.component';
+
 
 @Component({
   selector: 'app-fruitlist',
   standalone: true,
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule, SinglefruitComponent],
   templateUrl: './fruitlist.component.html',
   styleUrls: ['./fruitlist.component.scss']
 })
-export class FruitlistComponent implements OnInit {
-  howITWorks = "not bad"
+export class FruitlistComponent {
+ 
+  fontColorGood = "green";
+  fontColorBad = "red";
 
   fruitlist = [
     {
-      name: "Apfel",
-      img:"apple.png",
-      description: "Äpfel sind aufgrund ihres hohen Wassergehalts kalorienarm und enthalten nur Spuren von Fett und Eiweiß, dafür aber rund zwei Prozent Ballaststoffe und etwa elf Prozent Kohlenhydrate. Äpfel enthalten auch viele Vitamine und Mineralstoffe und sind daher eine wichtige Quelle für uns - zum Beispiel für Vitamin C.",
-      genus: "Kernobstgewächsen innerhalb der Familie der Rosengewächse",
-      stars: 2.3,
-      reviews:[{name: "Kevin W.", text: "ist lecker"},{name: "Arne P.", text: "nicht so meins"}],
+      
     },
     {
       name: "Banane",
@@ -63,6 +60,4 @@ export class FruitlistComponent implements OnInit {
       reviews:[{name: "Kevin W.", text: "Ganz gut in der Regel!"},{name: "Oliver J.", text: "Absoluter Favorit beim Frühstück"}],
     },
   ]
-  ngOnInit() {
-  } 
 }
