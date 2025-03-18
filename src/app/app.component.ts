5340ae0f62413d02e51024b35e6442b7765b35d8
MihaelaAghirculesei <aghirculesei@gmail.com>
2025-03-18 16:35:55 +0100

    Änderungen im Submodul 'fruitbasket'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './fruitlist/shared/footer/header/header.component';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './fruitlist/shared/footer/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ HeaderComponent, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
